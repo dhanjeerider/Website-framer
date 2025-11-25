1e2271736a796673061bb43bc3de4da632b12741
Dhanjee Kumar <102199585+dhanjeerider@users.noreply.github.com>
2025-11-25 08:45:20 +0000

    fgj

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** Minimal Next.js config for static export */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  // If you rely entirely on next export output:
   output: "export",
-  // ...existing config if any...
 };
-
-export default nextConfig;
